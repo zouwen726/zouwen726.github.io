7f30f13aa07050f444a4987f202ff8e0b22f4242
zouwen726 <2535685121@qq.com>
2022-01-27 10:17:08 +0800

    Site updated: 2022-01-27 10:17:07

--- js/xuehuapiaoluo.js
+++ js/xuehuapiaoluo.js
@@ -1,5 +1,6 @@
 /*样式一*/
 //背景雪花飘落特效
+
 (function($){
     $.fn.snow = function(options){
     var $flake = $('<div id="snowbox" />').css({'position': 'absolute','z-index':'9999', 'top': '-50px'}).html('&#10052;'),
@@ -38,6 +39,6 @@
       $.fn.snow({ 
         minSize: 5, /* 定义雪花最小尺寸 */
         maxSize: 50,/* 定义雪花最大尺寸 */
-        newOn: 500  /* 定义密集程度，数字越小越密集 */
+        newOn: 300  /* 定义密集程度，数字越小越密集 */
       });
   });
